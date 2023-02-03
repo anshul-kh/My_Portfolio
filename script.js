b6c459659dacd53c332376e46fc2ac895cbb9f13
Anshul <kh.anshul04@gmail.com>
2023-02-03 17:45:23 +0000

    Resolved JavaScript Error

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  
+    //hamburger Toggle
+    $('.humbarger').click(function(event){
+    $('.menu-list').slideToggle(100);
+    event.preventDefault();
+    
+    $('.menu-list li a').click(function(event) {
+        if ($(window).width() < 768) {
+          $('.menu-list').slideUp(100);
+          event.preventDefault(); 
+        }
+      });
+    });
+    
+    });
